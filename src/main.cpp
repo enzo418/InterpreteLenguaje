943a9e7dcbb4e792684ce4dd9b92f67eda7f0811
Enzo Aguirre <c.enzoaguirre@gmail.com>
2020-07-18 22:30:47 -0300

    + Ajustada la gramatica a los nuevos cambios + Agregado una funcion para pasar el arbol a un archivo

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,50 @@
 #include <iostream>
 #include <functional>           // std::ref
 
+#include <sstream>
+#include <fstream>
+
 using Complex=AnalizadorLexico::ComponenteLexico;
+
+void RaizAString(AnalizadorSintactico::Nodo* raiz, std::string& texto){
+	std::ostringstream dirraiz;
+	dirraiz << (void const *)raiz;
+
+	texto += dirraiz.str() + "_" + raiz->contenido + ":";
+
+	// agregar todos los hijos a la cadena separados por $
+	size_t sz = raiz->hijos.size(); 
+    // apilar todos los simbolos (de derecha a izquierda) y crear sus nodos
+    for(int i = sz-1; i >= 0; i--) {
+        std::ostringstream dirhijo;
+	    dirhijo << (void const *)raiz->hijos[i];
+		texto += "[" + dirhijo.str() +"_"+ raiz->hijos[i]->contenido + "]$";
+	}
+
+    if(sz > 0)
+        // remplezar el ultimo $ con \n
+        texto[texto.length() - 1] = '\n';
+    else
+        texto += "\n";
+
+	// Para cada hijo de esta raiz hacer lo mismo
+	for(AnalizadorSintactico::Nodo* nodo: raiz->hijos){
+		RaizAString(nodo, texto);
+	}
+}
+
+void ArbolAArchivo(AnalizadorSintactico::Nodo* arbol){
+    std::ifstream archivo;
+	std::string texto = "";
+	
+	RaizAString(arbol, texto);
+
+	std::ofstream outfile ("arbol.txt");
+
+	outfile << texto << std::endl;
+
+	outfile.close();	    
+}
 
 int main(){
 	AnalizadorSintactico::TAS tas = 
@@ -28,7 +71,7 @@ int main(){
         { 	{"<Condiciones>",     Complex::Id},						{"<OpAritmeticas>", "opRel", "<OpAritmeticas>", "<A>"} },
         { 	{"<Condiciones>",     Complex::Constante},				{"<OpAritmeticas>", "opRel", "<OpAritmeticas>", "<A>"} },
      /* { 	{"<Condiciones>",     Complex::ParantesisA},			{"<OpAritmeticas>", "opRel", "<OpAritmeticas>", "<A>"} },*/
-        { 	{"<Condiciones>",     Complex::ParantesisA},			{"(", "<OpAritmeticas>", "opRel", "<OpAritmeticas>", "<A>", ")", "<A>"} },
+        { 	{"<Condiciones>",     Complex::ParantesisA},			{"(", "<Condiciones>", ")", "<A>"} },
         { 	{"<A>",               Complex::And},					{"and", "<Condiciones>"} },
         { 	{"<A>",               Complex::Or},						{"or", "<Condiciones>"} },
         { 	{"<A>",               Complex::ParentesisC},			{"epsilon"} },     
@@ -70,6 +113,8 @@ int main(){
 
     int codigo = ObtenerArbolDerivacion(arbol, std::ref(tas), SimboloInicial);
     
+    ArbolAArchivo(arbol);
+
     // por ahora limpiamos el arbol, si quisieramos utilizar el interprete tendriamos que guardarlo.
 	LimpiarArbol(arbol);
     
